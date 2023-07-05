13f05079e170f45956c46cd50e7d5dce9390e3ff
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-05 20:18:09 +0100

    feat: add all controllers

---
+++ Angular/cake-house--project/server/src/controllers/cakeController.ts
@@ -0,0 +1,7 @@
+import * as cakeService from '../services/cakeService';
+import * as factoryController from './factoryController';
+
+export const createOne = factoryController.createOne(cakeService);
+export const getAll = factoryController.getAll(cakeService);
+export const updateOne = factoryController.updateOne(cakeService);
+export const deleteOne = factoryController.deleteOne(cakeService);
