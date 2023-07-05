07e028a18e6577ebefb711c7f315d6a7b88e4d34
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-05 11:37:33 +0100

    feat: add login route

--- Angular/cake-house--project/server/src/routes/userRouter.ts
+++ Angular/cake-house--project/server/src/routes/userRouter.ts
@@ -4,5 +4,6 @@ import * as authService from '../controllers/authController';
 const router = Router();
 
 router.route('/register').post(authService.register);
+router.route('/login').get(authService.login);
 
 export default router;
