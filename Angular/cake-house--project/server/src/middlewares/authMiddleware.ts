7567323d363b0a8636fbd4dea02bafc4e95bfc02
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-05 19:56:59 +0100

    feat: restrict middleware

--- Angular/cake-house--project/server/src/middlewares/authMiddleware.ts
+++ Angular/cake-house--project/server/src/middlewares/authMiddleware.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import jwt from '../lib/jsonwebtoken';
 import catchAsync from '../utils/catchAsync';
 import { Request, Response, NextFunction } from 'express';
-
+import AppError from '../utils/AppError';
 
 dotenv.config({ path: './config.env' });
 
@@ -23,3 +23,17 @@ export const authentication = catchAsync(
     next();
   }
 );
+
+export const restrictTo = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    if (!req.user) {
+      return next(new AppError('You are not logged in', 403));
+    }
+    if (req.user.role && !roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+    next();
+  };
+};
