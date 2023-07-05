34a4c3202cb5dcdd45a79fe6215db2699363b5d7
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-05 20:06:38 +0100

    feat: use deleteOne factoryController

--- Angular/cake-house--project/server/src/controllers/candyController.ts
+++ Angular/cake-house--project/server/src/controllers/candyController.ts
@@ -4,3 +4,4 @@ import * as factoryController from './factoryController';
 export const createOne = factoryController.createOne(candyService);
 export const getAll = factoryController.getAll(candyService);
 export const updateOne = factoryController.updateOne(candyService);
+export const deleteOne = factoryController.deleteOne(candyService);
