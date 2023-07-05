b348f19db6b95277b2ab060a3a35487adf7ffb10
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-05 19:08:28 +0100

    feat: auauthentication and productsRouter

--- Angular/cake-house--project/server/src/app.ts
+++ Angular/cake-house--project/server/src/app.ts
@@ -1,14 +1,19 @@
 import express, { ErrorRequestHandler } from 'express';
 
 import userRouter from './routes/userRouter';
+import productsRouter from './routes/productsRouter';
 import errorHandler from './controllers/errorController';
+import { authentication } from './middlewares/authMiddleware';
 
 const app = express();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
+app.use(authentication);
+
 app.use('/user', userRouter);
+app.use('/products/', productsRouter);
 
 app.use(errorHandler as ErrorRequestHandler);
 export default app;
