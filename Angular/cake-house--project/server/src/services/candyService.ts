5e0c1322328c1e9eb884c1666d39fbef88344148
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-05 19:58:08 +0100

    feat: use update factoryService

--- Angular/cake-house--project/server/src/services/candyService.ts
+++ Angular/cake-house--project/server/src/services/candyService.ts
@@ -3,4 +3,4 @@ import Candy from '../models/candyModel';
 
 export const createOne = factoryServices.createOne(Candy);
 export const getAll = factoryServices.getAll(Candy);
-
+export const updateOne = factoryServices.updateOne(Candy);
