520f17239f7329c83adb3aea0bec4357d53888ab
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-05 19:18:03 +0100

    feat: use getAll factoryService

--- Angular/cake-house--project/server/src/services/candyService.ts
+++ Angular/cake-house--project/server/src/services/candyService.ts
@@ -2,3 +2,5 @@ import * as factoryServices from './factoryService';
 import Candy from '../models/candyModel';
 
 export const createOne = factoryServices.createOne(Candy);
+export const getAll = factoryServices.getAll(Candy);
+
