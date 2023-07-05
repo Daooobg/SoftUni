6ae543663ed77f00230cf4d70b16acf2e1518ca6
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-05 19:03:37 +0100

    feat: declare express interface

--- Angular/cake-house--project/server/src/utils/catchAsync.ts
+++ Angular/cake-house--project/server/src/utils/catchAsync.ts
@@ -1,5 +1,18 @@
 import { RequestHandler } from 'express';
 
+declare module 'express' {
+  interface Request {
+    user: {
+      _id?: string;
+      role?: string;
+      name?: string;
+      email?: string;
+      // Add other properties of the user object
+    };
+  }
+}
+
+
 const catchAsync = (fn: Function): RequestHandler => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
