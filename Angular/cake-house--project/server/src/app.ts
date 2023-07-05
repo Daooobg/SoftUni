19e5bc9c57a87c059e924b6cac5174bccdea7b5b
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-05 11:30:45 +0100

    feat: add user router

--- Angular/cake-house--project/server/src/app.ts
+++ Angular/cake-house--project/server/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
+import userRouter from './routes/userRouter';
 
 const app = express();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/user', userRouter);
 
 export default app;
