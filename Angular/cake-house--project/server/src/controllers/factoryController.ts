2c8739b795901b810335d3e37688041a971a6d5a
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-05 19:18:45 +0100

    feat: getAll factory controller

--- Angular/cake-house--project/server/src/controllers/factoryController.ts
+++ Angular/cake-house--project/server/src/controllers/factoryController.ts
@@ -3,9 +3,11 @@ import { NextFunction, Request, Response } from 'express';
 
 import catchAsync from '../utils/catchAsync';
 import AppError from '../utils/AppError';
+import { QueryString } from '../utils/ApiFeatures';
 
 interface Services {
   createOne: (data: {}, objectId: Types.ObjectId) => Promise<object | null>;
+  getAll: (query: QueryString) => Promise<object[]>;
 }
 
 export const createOne = <T extends Services>(ModelService: T) =>
@@ -21,6 +23,17 @@ export const createOne = <T extends Services>(ModelService: T) =>
 
     res.status(201).json({
       status: 'success',
+      data: data,
+    });
+  });
+
+export const getAll = <T extends Services>(ModelService: T) =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const data = await ModelService.getAll(req.query as QueryString);
+
+    res.status(200).json({
+      status: 'success',
+      result: data.length,
       data: data,
     });
   });
