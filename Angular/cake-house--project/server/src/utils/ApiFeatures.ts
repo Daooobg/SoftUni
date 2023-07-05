681c3e6c5bec9040a68b6cc83b974f40a93c0f49
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-05 11:51:56 +0100

    feat: Api query

---
+++ Angular/cake-house--project/server/src/utils/ApiFeatures.ts
@@ -0,0 +1,64 @@
+export  interface QueryString {
+  [key: string]: string | undefined;
+  page?: string;
+  sort?: string;
+  limit?: string;
+  fields?: string;
+}
+
+
+export class APIFeatures {
+  query: any;
+  queryString: QueryString;
+
+  constructor(query: any, queryString: QueryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //2) Advanced filtering
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+    this.query = this.query.find(JSON.parse(queryString));
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      console.log('sort');
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAT');
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+
+  paginate() {
+    const page = Number(this.queryString.page) || 1;
+    const limit = Number(this.queryString.limit) || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
