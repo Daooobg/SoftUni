dad0be5b50bea88151df2bdfd68e850144e88df3
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-26 14:53:17 +0100

    feat: alergen section

---
+++ Angular/cake-house--project/app/src/app/home/home-allergen/home-allergen.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-allergen',
+  templateUrl: './home-allergen.component.html',
+  styleUrls: ['./home-allergen.component.css'],
+})
+export class HomeAllergenComponent {}
