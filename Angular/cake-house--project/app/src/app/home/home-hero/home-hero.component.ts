e2b8130f398b02a3c840822db2d19a4c90602728
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-26 13:40:28 +0100

    feat: hero section

---
+++ Angular/cake-house--project/app/src/app/home/home-hero/home-hero.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-hero',
+  templateUrl: './home-hero.component.html',
+  styleUrls: ['./home-hero.component.css'],
+})
+export class HomeHeroComponent {}
