632887072e62e98d033d9c77ae87c85d68a4358c
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-26 13:41:08 +0100

    feat: about products section

---
+++ Angular/cake-house--project/app/src/app/home/home-about/home-about.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-about',
+  templateUrl: './home-about.component.html',
+  styleUrls: ['./home-about.component.css'],
+})
+export class HomeAboutComponent {}
