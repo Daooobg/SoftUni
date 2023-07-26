35261f19eddea013b1a6483c0ff12336b4b43e79
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-26 14:53:59 +0100

    feat: home components

--- Angular/cake-house--project/app/src/app/app.module.ts
+++ Angular/cake-house--project/app/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { HomeAboutComponent } from './home/home-about/home-about.component';
+import { HomeHeroComponent } from './home/home-hero/home-hero.component';
+import { HomeAllergenComponent } from './home/home-allergen/home-allergen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    HomeAboutComponent,
+    HomeHeroComponent,
+    HomeAllergenComponent,
   ],
   imports: [
     BrowserModule,
