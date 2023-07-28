13f72f82fc359c4184af4cab41d6c79474d58dfe
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-28 12:14:24 +0100

    update

--- Angular/cake-house--project/app/src/app/app.component.ts
+++ Angular/cake-house--project/app/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from './store/app.reducer';
+import * as AuthActions from './auth/store/auth.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private store: Store<fromApp.AppStore>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(AuthActions.autoLogin());
+  }
+
+  
 }
--- Angular/cake-house--project/app/src/app/app.module.ts
+++ Angular/cake-house--project/app/src/app/app.module.ts
@@ -10,7 +10,12 @@ import { HomeAboutComponent } from './home/home-about/home-about.component';
 import { HomeHeroComponent } from './home/home-hero/home-hero.component';
 import { HomeAllergenComponent } from './home/home-allergen/home-allergen.component';
 import { AuthComponent } from './auth/auth.component';
-
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromAuth from './auth/store/auth.reducer';
+import { AuthEffects } from './auth/store/auth.effects';
+import { HttpClientModule } from '@angular/common/http';
+import { LoadingSpinnerComponent } from './util/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
@@ -21,8 +26,16 @@ import { AuthComponent } from './auth/auth.component';
     HomeHeroComponent,
     HomeAllergenComponent,
     AuthComponent,
+    LoadingSpinnerComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    StoreModule.forRoot({ auth: fromAuth.authReducer }),
+    EffectsModule.forRoot([AuthEffects]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
