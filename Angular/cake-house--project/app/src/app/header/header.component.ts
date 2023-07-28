2ea297c582749b6d38f3b7072e33ec9b86fab53c
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-28 12:13:41 +0100

    feat: add logout and update links

--- Angular/cake-house--project/app/src/app/header/header.component.ts
+++ Angular/cake-house--project/app/src/app/header/header.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription, map } from 'rxjs';
+
+import * as fromApp from '../store/app.reducer';
+import { User } from '../auth/user.model';
+import * as AuthActions from '../auth/store/auth.actions';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSubs!: Subscription;
+  user!: User | null;
 
+  constructor(private store: Store<fromApp.AppStore>) {}
+  ngOnInit(): void {
+    this.userSubs = this.store
+      .select('auth')
+      .pipe(map((authState) => authState.user))
+      .subscribe((user) => {
+        this.isAuthenticated = !!user;
+      });
+  }
+  ngOnDestroy(): void {
+    this.userSubs.unsubscribe();
+  }
+
+  onLogout() {
+    localStorage.removeItem('userData');
+    this.store.dispatch(AuthActions.logout());
+  }
 }
