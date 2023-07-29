dc6a9182ae926c71c1f7314aaf9327c140460f58
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-29 14:17:41 +0100

    feat: add create product route

--- Angular/cake-house--project/app/src/app/app-routing.module.ts
+++ Angular/cake-house--project/app/src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
 import { ProductsComponent } from './products/products.component';
+import { ProductCreateComponent } from './products/product-create/product-create.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'auth', component: AuthComponent },
-  { path: 'products', component: ProductsComponent },
+  {
+    path: 'products',
+
+    children: [
+      { path: '', component: ProductsComponent },
+      { path: 'new', component: ProductCreateComponent },
+    ],
+  },
 ];
 
 @NgModule({
