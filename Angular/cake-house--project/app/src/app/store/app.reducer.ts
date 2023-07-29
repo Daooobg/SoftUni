4a69fab110283f569802c66191f939ca206e4994
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-29 09:09:15 +0100

    feat: add products store

--- Angular/cake-house--project/app/src/app/store/app.reducer.ts
+++ Angular/cake-house--project/app/src/app/store/app.reducer.ts
@@ -1,5 +1,6 @@
 import * as fromAuth from '.././auth/store/auth.reducer';
-
+import * as fromProducts from '../products/store/products.reducer';
 export interface AppStore {
   auth: fromAuth.State;
+  products: fromProducts.State;
 }
