7ee8b145dfb1831faec6c6465aaa4342e26cc354
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-29 09:08:43 +0100

    feat: add product route

--- Angular/cake-house--project/app/src/app/app-routing.module.ts
+++ Angular/cake-house--project/app/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'auth', component: AuthComponent },
+  { path: 'products', component: ProductsComponent },
 ];
 
 @NgModule({
