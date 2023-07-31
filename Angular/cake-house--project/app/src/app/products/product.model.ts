acf03773da28ab7840285684f6bc17ab2f6d5ce3
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-07-31 09:02:17 +0100

    feat: add properies

--- Angular/cake-house--project/app/src/app/products/product.model.ts
+++ Angular/cake-house--project/app/src/app/products/product.model.ts
@@ -1,11 +1,16 @@
 export class Product {
   constructor(
-    public productId: string,
-    public productName: string,
     public description: string,
     public images: string[],
     public price: number,
     public types: string[],
-    public name: string
+    public name: string,
+    public sizes: string[],
+    public createAt?: string,
+    public ownerId?: string,
+    public priceDiscount?: number,
+    public product?: string,
+    public slug?: string,
+    public _id?: string
   ) {}
 }
